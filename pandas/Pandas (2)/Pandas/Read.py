5ef910feabeed1abad5fbfd8bb8cb2c6b3310317
Goofyparag <paragt2005@gmail.com>
2025-11-16 02:24:09 +0530

    Save working-tree changes on demo-branch before merging to main

--- pandas/Pandas (2)/Pandas/Read.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-RF = pd.read_csv("sales_data_sample.csv", encoding="latin1")
-# RF = pd.read_excel('SampleSuperstore.xlsx')
-RF = pd.read_json('Sample_Data.json')
-
-print(RF)
-
-
